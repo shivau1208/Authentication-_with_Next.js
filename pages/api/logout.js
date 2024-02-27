421d884aa4d99cde6bdb867550d8ae61e4006fe9
Shivaraj Umadi <shivaumadi@outlook.com>
2024-02-27 18:49:31 +0000

    some debugs

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -1,10 +1,10 @@
-import { sessionCookie } from "../../lib/request";
-import {withIronSessionApiRoute} from "iron-session/next"
+// import { sessionCookie } from "../../lib/request";
+// import {withIronSessionApiRoute} from "iron-session/next"
 
-export default withIronSessionApiRoute(
-    function logoutRoute(req,res,session) {
-        req.session.destroy();
-        res.send({status:"success"});
-    },
-    sessionCookie()
-)
+// export default withIronSessionApiRoute(
+//     function logoutRoute(req,res,session) {
+//         req.session.destroy();
+//         res.send({status:"success"});
+//     },
+//     sessionCookie()
+// )
